c4feed852b8c5cff5be82fa6d832b0f6968560e3
abitarecotool <billy.dolor@abitareco.it>
2026-07-14 10:26:38 +0200

    Fix Supabase anon key

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window.ABITARE_CONFIG = {
   SUPABASE_URL: 'https://ddakythxpllinofstzuh.supabase.co',
-  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRkYWt5dGh4cGxsaW5vZnN0enVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM5NTE4MDYsImV4cCI6MjA5OTUyNzgwNn0.UBlsk4xcQa-6LWSE7cAVnD-gP3dX3auwDsR4hXiP70Ue',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRkYWt5dGh4cGxsaW5vZnN0enVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM5NTE4MDYsImV4cCI6MjA5OTUyNzgwNn0.UBlsk4xcQa-6LWSE7cAVnD-gP3dX3auwDsR4hXiP70U',
   APP_NAME: 'Abitare Co. Lavorazioni',
   ADMIN_EMAILS: ['billy.dolor@abitareco.it','mattia.nichettistanghellini@abitareco.it'],
   NOTIFY_EMAILS: ['billy.dolor@abitareco.it','mattia.nichettistanghellini@abitareco.it']
